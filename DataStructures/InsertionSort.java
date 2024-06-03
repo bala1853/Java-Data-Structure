dd6301c483ea0b2dda3b8eabf27821d343653181
bala1853 <89856585+bala1853@users.noreply.github.com>
2024-06-03 20:46:20 +0530

    Add files via upload

---
+++ DataStructures/InsertionSort.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 3, 1 };
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else {
+                    break;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
